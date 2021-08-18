e246b38177dfc7ed42bd93766c0118b24da274a8
Simge Erdil <siisi4officiel@gmail.com>
2021-08-18 04:45:35 +0200

    pick up script complete

---
+++ Assets/Scripts/Cubes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cubes : MonoBehaviour
+{
+    int OnOff = 0;
+    public GameObject CubeRed;
+    public GameObject MyCube;
+    public Color MyColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.tag == "Player" || OnOff == 0){
+
+            // L'objet disparait 
+            CubeRed.SetActive(false);
+            // On a qqch en main
+            OnOff += 1;
+            // Mon cube prend la couleur de l'objet
+            MyCube.GetComponent<Renderer>().material.color = MyColor;
+            
+
+            Debug.Log("Object is picked up");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

9bca6f47d5ef769096e287708ea1fc2f5d3eb57c
Simge Erdil <siisi4officiel@gmail.com>
2021-08-19 00:04:01 +0200

    elevator down

--- Assets/Scripts/ElevatorMax.cs
+++ Assets/Scripts/ElevatorMax.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class ElevatorMax : MonoBehaviour
 {
-    public GameObject Elevator;
-    //public GameObject limit;
+    public Transform target;
+    //public float t;
+    public float speed;
+    
+    //public GameObject Elevator;
+    public Transform limit;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("limit")){
-            Elevator.transform.Translate(0f, -1f * Time.deltaTime, 0f);
+    //private void OnTriggerEnter(Collider other) {
+    //    if(other.CompareTag("limit")){
+    //        Elevator.transform.Translate(0f, -1f * Time.deltaTime, 0f);
+    //    }
+    //}
+    
+    void FixedUpdate() {
+        Vector3 a = transform.position;
+        Vector3 b = target.position;
+        if(Vector3.Distance(transform.position, limit.position) <= 5){
+            transform.position = Vector3.MoveTowards(a, b, speed);
+            Debug.Log("hitted max");
         }
+        
     }
     
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
